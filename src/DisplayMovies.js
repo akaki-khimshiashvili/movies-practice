1b066e531b368b1cc3a03d214f110a85694263ab
Akaki Khimshiashvili <kakikhimshiashvili@gmail.com>
2025-03-16 00:27:12 +0400

    Final Commit

--- src/DisplayMovies.js
+++ src/DisplayMovies.js
@@ -1,26 +1,15 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Movies from "./Movies";
 
 export function DisplayMovies() {
-  const [movies, setMovies] = useState([
-    { id: 1, title: "Inception", year: 2010, genre: "Sci-Fi", rating: 8.8 },
-    {
-      id: 2,
-      title: "The Dark Knight",
-      year: 2008,
-      genre: "Action",
-      rating: 9,
-    },
-    { id: 3, title: "Interstellar", year: 2014, genre: "Sci-Fi", rating: 8.6 },
-    { id: 4, title: "Parasite", year: 2019, genre: "Thriller", rating: 8.6 },
-    {
-      id: 5,
-      title: "The Shawshank Redemption",
-      year: 1994,
-      genre: "Drama",
-      rating: 9.3,
-    },
-  ]);
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch("/movies.json")
+      .then((response) => response.json()) // Convert the response to JSON
+      .then((data) => setMovies(data)) // Set the fetched data into state
+      .catch((error) => console.error("Error Fetching data ", error));
+  }, []);
 
   function handleDelete(id) {
     const updatedMovies = movies.filter((movie) => movie.id !== id);
